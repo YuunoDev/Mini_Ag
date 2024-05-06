9c821f2e590fbcd5a68f258f7513cbae7f98a726
YuunoDev <adrianalonso.a4@proton.me>
2024-05-06 10:23:44 -0600

    Update CSS for carsl, home, and menu components, cars images

--- src/app/ourCars.ts
+++ src/app/ourCars.ts
@@ -1,7 +1,8 @@
 import { Car } from "./car";
 
 export const OurCars: Car[] = [
-    {   id: 0, 
+    {   
+        id: 0, 
         img: '../../assets/images/car0.jpg', 
         name: 'BMW Serie 5 4 PTS 540I M SPORT, TA', 
         year: 2018, 
@@ -9,16 +10,101 @@ export const OurCars: Car[] = [
         'Con su diseño atlético, motor TwinPower Turbo de seis cilindros y el paquete M Sport que realza'+
         ' su dinamismo, ofrece una experiencia de conducción emocionante. Equipado con tecnología de vanguardia'+
         'y características de seguridad avanzadas, este vehículo garantiza comodidad y rendimiento en cada viaje.', 
-        pricerent: 100, 
+        pricerent: 80, 
         isRented: true, 
-        brand: 'BMW' },
-    { id: 1, img: 'car1.jpg', name: 'Audi', year: 2016, description: 'white', pricerent: 150, isRented: false, brand: 'Audi' },
-    { id: 2, img: 'car2.jpg', name: 'Mercedes', year: 2015, description: 'gray', pricerent: 200, isRented: false, brand: 'Mercedes' },
-    { id: 3, img: 'car3.jpg', name: 'Toyota', year: 2017, description: 'red', pricerent: 80, isRented: false, brand: 'Toyota' },
-    { id: 4, img: 'car4.jpg', name: 'Ford', year: 2019, description: 'blue', pricerent: 90, isRented: false, brand: 'Ford' },
-    { id: 5, img: 'car5.jpg', name: 'Chevrolet', year: 2014, description: 'black', pricerent: 70, isRented: false, brand: 'Chevrolet' },
-    { id: 6, img: 'car6.jpg', name: 'Nissan', year: 2013, description: 'white', pricerent: 60, isRented: false, brand: 'Nissan' },
-    { id: 7, img: 'car7.jpg', name: 'Hyundai', year: 2012, description: 'gray', pricerent: 50, isRented: false, brand: 'Hyundai' },
-    { id: 8, img: 'car8.jpg', name: 'Honda', year: 2011, description: 'red', pricerent: 40, isRented: false, brand: 'Honda' },
-    { id: 9, img: 'car9.jpg', name: 'Kia', year: 2010, description: 'blue', pricerent: 30, isRented: false, brand: 'Kia' }
+        brand: 'BMW' 
+    },
+    {   
+        id: 1, 
+        img: '../../assets/images/car1.jpg', 
+        name: 'Audi A3 1.8 TFSI AMBIENTE S TRONIC Sedan 2016 white', 
+        year: 2016, 
+        description: 'El Audi A3 1.8 TFSI Ambiente S Tronic Sedán 2016 combina estilo y rendimiento de manera excepcional.'+
+        ' Con su diseño sofisticado, motor TFSI de 1.8 litros y transmisión S Tronic, ofrece una experiencia de conducción'+
+        ' emocionante y suave. Equipado con tecnología avanzada y características de seguridad destacadas, este sedán garantiza'+
+        ' comodidad y confiabilidad en cada viaje.', 
+        pricerent: 50, 
+        isRented: false, 
+        brand: 'Audi' 
+    },
+    {   
+        id: 2, 
+        img: '../../assets/images/car2.jpg', 
+        name: 'Mercedes-Benz Clase CLA 2.0 250 Cgi Sport Mt', 
+        year: 2015, 
+        description: 'El Mercedes-Benz Clase CLA 250 CGI Sport MT es la fusión perfecta de elegancia y rendimiento. Con su diseño distintivo y motor potente de 2.0 litros, ofrece una experiencia de conducción emocionante. Equipado con tecnología avanzada y características de seguridad destacadas, este sedán garantiza una experiencia de conducción excepcional en cada viaje.', 
+        pricerent: 70, 
+        isRented: false, 
+        brand: 'Mercedes' 
+    },
+    {   
+        id: 3, 
+        img: '../../assets/images/car3.jpg', 
+        name: 'Toyota Prius 1.8 Premium Sr Hibrido At', 
+        year: 2017, 
+        description: 'El Toyota Prius 1.8 Premium SR Híbrido AT ofrece una experiencia de conducción eficiente y moderna. Con su motor híbrido de 1.8 litros, este sedán proporciona un rendimiento económico y respetuoso con el medio ambiente. Equipado con características premium y tecnología avanzada, el Prius garantiza comodidad y confiabilidad en cada viaje.', 
+        pricerent: 80, 
+        isRented: false, 
+        brand: 'Toyota' 
+    },
+    { 
+        id: 4, 
+        img: '../../assets/images/car4.jpg', 
+        name: 'Ford Fiesta 1.6 S Sedan At', 
+        year: 2019, 
+        description: 'El Ford Fiesta 1.6 S Sedán AT en color rojo es un automóvil compacto que combina estilo y rendimiento. Con su motor de 1.6 litros y transmisión automática, ofrece una conducción ágil y eficiente. Su diseño moderno y deportivo, junto con su color vibrante, lo hacen destacar en cualquier camino. Ideal para quienes buscan un vehículo práctico y con estilo.', 
+        pricerent: 30, 
+        isRented: false, 
+        brand: 'Ford' 
+    },
+    { 
+        id: 5, 
+        img: '../../assets/images/car5.jpg', 
+        name: 'Chevrolet Cruze 1.8 A Ls Aa Cd Mp3 R-16 At', 
+        year: 2014, 
+        description: 'El Chevrolet Cruze 1.8 A LS AA CD MP3 R-16 AT es un sedán versátil y confiable. Con su motor de 1.8 litros y transmisión automática, ofrece un rendimiento suave y eficiente en la carretera. Equipado con características como aire acondicionado, reproductor de CD y MP3, este vehículo brinda comodidad y entretenimiento en cada viaje. Ideal para aquellos que buscan un automóvil práctico y funcional.', 
+        pricerent: 40, 
+        isRented: false, 
+        brand: 'Chevrolet' 
+    },
+    { 
+        id: 6, 
+        img: '../../assets/images/car6.jpeg', 
+        name: 'Nissan Kicks 2018', 
+        year: 2018, 
+        description: 'El Nissan Kicks 2018 es un SUV compacto que ofrece estilo y versatilidad. Con su diseño moderno y características innovadoras, es perfecto para la vida urbana. Equipado con tecnología avanzada y un amplio espacio interior, brinda comodidad y conveniencia en cada viaje. Ideal para aquellos que buscan un vehículo ágil y práctico para sus aventuras diarias.', 
+        pricerent: 50, 
+        isRented: false, 
+        brand: 'Nissan' 
+    },
+    { 
+        id: 7, 
+        img: '../../assets/images/car7.jpg', 
+        name: 'Hyundai Creta 2019', 
+        year: 2019, 
+        description: 'La Hyundai Creta 2019 es una SUV compacta que combina estilo y funcionalidad. Con su diseño moderno y características avanzadas, es perfecta para la vida urbana y las aventuras en carretera. Equipada con tecnología innovadora y un amplio espacio interior, ofrece comodidad y versatilidad para toda la familia. Ideal para quienes buscan un vehículo confiable y elegante para sus desplazamientos diarios.', 
+        pricerent: 40, 
+        isRented: false, 
+        brand: 'Hyundai' 
+    },
+    { 
+        id: 8, 
+        img: '../../assets/images/car8.jpeg', 
+        name: 'Honda City 1.5 Ex Cvt', 
+        year: 2017, 
+        description: 'El Honda City 1.5 EX CVT es un sedán compacto que combina rendimiento y comodidad. Con su motor de 1.5 litros y transmisión CVT, ofrece una conducción suave y eficiente en el consumo de combustible. Equipado con características premium y tecnología avanzada, como una pantalla táctil y conectividad Bluetooth, brinda una experiencia de conducción moderna y conveniente. Ideal para aquellos que buscan un vehículo confiable y elegante para sus desplazamientos diarios.', 
+        pricerent: 40, 
+        isRented: false, 
+        brand: 'Honda' 
+    },
+    { 
+        id: 9, 
+        img: '../../assets/images/car9.jpg', 
+        name: 'Kia Sorento 2018', 
+        year: 2018, 
+        description: 'El Kia Sorento 2018 es un SUV versátil y confiable. Con su diseño robusto y elegante, ofrece un equilibrio perfecto entre estilo y funcionalidad. Equipado con características avanzadas de seguridad y tecnología de última generación, como sistemas de asistencia al conductor y conectividad Bluetooth, brinda comodidad y tranquilidad en cada viaje. Con un amplio espacio interior y capacidad para siete pasajeros, es ideal para familias y aventuras en carretera.', 
+        pricerent: 50, 
+        isRented: false, 
+        brand: 'Kia' 
+    }
 ];
