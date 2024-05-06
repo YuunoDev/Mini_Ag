f57efcc030612d3d876298864ebc2aec1a1f9136
YuunoDev <adrianalonso.a4@proton.me>
2024-05-05 20:40:45 -0600

    chore: Update title and favicon to reflect new branding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { CarsService } from '../cars.service';
+import { DomseguroPipe } from '../domseguro.pipe';
+import { RouterOutlet } from '@angular/router';
+import { Car } from '../car';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DomseguroPipe, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  ourCars: Car[]= [];
+  video: string= '';
   constructor(private carsService: CarsService) { }
 
+  ngOnInit() {
+    this.ourCars = this.carsService.getCars();
+    this.video = 'HRMShnf1Ics';
+  }
+  
 }
