ff828207cc0069c381b095a1f8e1f7625a209d8b
Juan Rodo <aracisjuan@gmail.com>
2024-05-07 12:54:00 -0600

    mansory actualizado

--- src/app/layoutman/layoutman.component.ts
+++ src/app/layoutman/layoutman.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,OnInit,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-layoutman',
@@ -9,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class LayoutmanComponent {
   title = 'layoutman';
+  @ViewChild('container', { static: true }) container!: ElementRef<HTMLElement>;
+
+  ngOnInit() {
+    this.setup();
+  }
+
+  constructor() {} 
+  
+  setup() {
+    const numCols = 4;
+    const colHeights = Array(numCols).fill(1);
+    const container = this.container.nativeElement;
+
+    Array.from(container.children).forEach((child: any, i: number) => {
+      const order = i % numCols;
+      child.style.order = order;
+      colHeights[order] += parseFloat(child.clientHeight);
+    });
+    container.style.height = Math.max(...colHeights) + "px";
+  }
 }
