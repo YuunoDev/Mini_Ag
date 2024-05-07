f3a75f1ff94baecb5288251b5392dc61924a09bf
YuunoDev <adrianalonso.a4@proton.me>
2024-05-06 21:43:02 -0600

    feat: Update 'us' component to display team members dynamically

--- src/app/us/us.component.ts
+++ src/app/us/us.component.ts
@@ -1,12 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './us.component.html',
   styleUrl: './us.component.css'
 })
 export class UsComponent {
 
+  // Variable para controlar el modo oscuro
+  isDarkMode: boolean = false;
+
+  // Array de miembros del equipo
+  teamMembers: any[] = [
+    {
+      name: 'Adrian Alonso Arambula',
+      id: '322129',
+      quote: 'Ayuda hijos de la...',
+      image: '../../assets/images/AAA.jpeg'
+    },
+    {
+      name: 'José Luis Ornelas Valadez',
+      id: '262545',
+      quote: 'XD',
+      image: '../../assets/images/JLOV.jpeg'
+    },
+    {
+      name: 'Juan Rodolfo Aranda Cisneros',
+      id: '290589',
+      quote: 'Me dicen el comal porque caliento a las gorditas 🥴🥴',
+      image: '../../assets/images/JR.jpeg'
+    },
+    {
+      name: 'Andrés Heredia BallIn',
+      id: '226961',
+      quote: 'Murió como vivió, con una increíble adicción al fentanilo',
+      image: '../../assets/images/AHB.jpeg'
+    }
+  ];
 }
