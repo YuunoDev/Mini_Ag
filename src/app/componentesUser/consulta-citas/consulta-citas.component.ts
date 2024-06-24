279cf3cac2472b2ad0a54864e4f497e66179ab85
Andres Heredia <herediaandres040@gmail.com>
2024-06-24 11:21:04 -0600

    Arreglo redireccion en logout

--- src/app/componentesUser/consulta-citas/consulta-citas.component.ts
+++ src/app/componentesUser/consulta-citas/consulta-citas.component.ts
@@ -3,6 +3,7 @@ import { Datos } from '../../datos/datos.model';
 import { DatosService } from '../../datos/datos.service';
 import { UserService } from '../../user.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-consulta-citas',
@@ -15,17 +16,22 @@ export class ConsultaCitasComponent {
   datos!: Datos[];
   cuenta:any;
 
-  constructor(private datosService: DatosService, private userService:UserService) {
+  constructor(private datosService: DatosService, private userService:UserService, private router: Router) {
     const nombre = localStorage.getItem('userId');
     this.cuenta = nombre;
   }
 
   ngOnInit() {
     // this.datos = this.datosService.getDatos();
-    this.userService.getCitas().subscribe(datos => {
-      this.datos = datos;
-    })
-  }
+    if (!localStorage.getItem('userId')) {
+      this.router.navigate(['/home'])
+    }else{
+      this.userService.getCitas().subscribe(datos => {
+        this.datos = datos;
+      })
+    }
+    }
+    
 
   borrar(dato: Datos){
     this.userService.deleteCita(dato);
